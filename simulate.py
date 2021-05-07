1375c8190d51f911036629a2cdddca9c159fd1fc
jbongard <jbongard@.uvm.edu>
2021-05-07 15:54:30 -0400

    Floor has been added.

--- simulate.py
+++ simulate.py
@@ -1,7 +1,14 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-9.8)
+
+planeId = p.loadURDF("plane.urdf")
 
 p.loadSDF("box.sdf")
 
@@ -9,11 +16,11 @@ for t in range(0,1000):
 
    p.stepSimulation()
 
-   timeInSecondsToSleepFor = 1. / 60.
+   timeInSecondsToSleepFor = 1. / 600.
 
    time.sleep( timeInSecondsToSleepFor )
 
-   print('Timestep: ' , t )
+   # print('Timestep: ' , t )
 
 p.disconnect()
 
