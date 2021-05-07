e75618051138c455f1c7694d2baf681f08fc4034
jbongard <jbongard@.uvm.edu>
2021-05-07 15:23:05 -0400

    The simulation is stepping now.

--- simulate.py
+++ simulate.py
@@ -1,6 +1,17 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+for t in range(0,1000):
+
+   p.stepSimulation()
+
+   timeInSecondsToSleepFor = 1. / 60.
+
+   time.sleep( timeInSecondsToSleepFor )
+
+   print('Timestep: ' , t )
 
 p.disconnect()
 
